b1e8f6c3d9de1164e8968b1c68a1edb6a176ec34
Dennis Tenen <denten@gmail.com>
2013-11-11 03:47:50 +0000

    update the install script

--- symbootstrap.sh
+++ symbootstrap.sh
@@ -3,12 +3,13 @@
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 # Forked from https://github.com/michaeljsmalley/dotfiles
+# At this time, you must list the files manually
 
 ########## Variables
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
+files=".gitignore .gitmodules .i3 .screenlayout .vim .vimrc .Xmodemap .zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -22,7 +23,8 @@ echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
@@ -35,7 +37,7 @@ function install_zsh {
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     # Clone my oh-my-zsh repository from GitHub only if it isn't already present
     if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/michaeljsmalley/oh-my-zsh.git
+        git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh  
     fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
