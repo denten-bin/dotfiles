535f4f28c0cfc63f61dbba806785eb7edbadea10
Dennis Tenen <denten@gmail.com>
2013-11-11 05:00:03 +0000

    fix the script

--- symbootstrap.sh
+++ symbootstrap.sh
@@ -27,12 +27,12 @@ echo "done"
 # from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/$file ~/.dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/$file ~/$file
 done
 
-function install_zsh {
+install_zsh () {
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     # Clone my oh-my-zsh repository from GitHub only if it isn't already present
@@ -43,18 +43,14 @@ if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
         chsh -s $(which zsh)
     fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        sudo apt-get install zsh
+# else
+     # If zsh isn't installed, get the platform of the current machine
+     platform=$(uname);
+     # If the platform is Linux, try an apt-get to install zsh and then recurse
+     if [[ $platform == 'Linux' ]]; then
+         sudo apt-get install zsh
         install_zsh
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
-    fi
+     fi
 fi
 }
 
