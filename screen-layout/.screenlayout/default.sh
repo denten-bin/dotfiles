8d11d1e84b7328e6df991b4ec3ce79c708bffbd0
denten <denten@gmail.com>
2017-04-11 13:48:16 -0400

    update i3 and arandr layouts

--- screen-layout/.screenlayout/default.sh
+++ screen-layout/.screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-1 --mode 1440x900 --pos 1280x0 --rotate normal --output DP-1 --off --output eDP-1 --mode 1280x800 --pos 0x0 --rotate normal --output DP-2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
