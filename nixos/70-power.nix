2d89a9d0e0f5f3f59f451673a2724ae127691e85
denten <denten@gmail.com>
2018-06-28 20:47:45 -0400

    try to add the low bat rules

--- nixos/70-power.nix
+++ nixos/70-power.nix
@@ -19,4 +19,12 @@
       CPU_SCALING_GOVERNOR_ON_BAT=powersave
       ENERGY_PERF_POLICY_ON_BAT=powersave
     '';
+
+  services.udev.extraRules = ''
+       SUBSYSTEM="power_supply"
+       ATTR{status}="Discharging"
+       ATTR{capacity}="[0-5]"
+       RUN+="/usr/bin/systemctl hibernate"
+  '';
+
 }
