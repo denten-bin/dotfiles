6e70dc64f029584bc920f44886fc9b3864224364
denten <denten@gmail.com>
2021-10-23 23:28:10 -0400

    update for xresources for scaling

--- nixos/80-apps.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.bash.enableCompletion = true;
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-}
